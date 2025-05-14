4e7d13011baef48491f8f616e9e3e63baba8bfcd
Cesar <hakuchov2@gmail.com>
2025-05-14 01:03:24 -0500

    Ejercicios Tipos de errores - Error relativo

--- Tema1/ErrorRelativo/src/errorrelativo/ErrorRelativo.java
+++ Tema1/ErrorRelativo/src/errorrelativo/ErrorRelativo.java
@@ -4,7 +4,11 @@ package errorrelativo;
 public class ErrorRelativo {
 
     public static void main(String[] args) {
-        
+        double valorReal = 8.5;
+        double valorAproximado = 8.3;
+        double errorAbsoluto = Math.abs(valorReal - valorAproximado);
+        double errorRelativo = errorAbsoluto / Math.abs(valorReal);
+        System.out.println("Error Relativo: " + (errorRelativo * 100) + "%");
     }
     
 }
